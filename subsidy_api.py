73aaefb2f73c7dbb825ceb33e43fe603fd7ea9f1
anjaliwadhe001 <84492122+anjaliwadhe001@users.noreply.github.com>
2025-07-08 12:18:28 +0530

    Add files via upload

---
+++ subsidy_api.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, jsonify, send_file
+from subsidy_rajasthan import process_rajasthan
+from subsidy_haryana import process_haryana
+from subsidy_up_msme import process_up_msme
+from subsidy_uttarpradesh import process_up
+from subsidy_madhyapradesh import process_madhyapradesh
+from subsidy_karnataka import process_karnataka
+from subsidy_tamilnadu import process_tamilnadu 
+from subsidy_maharashtra import process_maharashtra
+from subsidy_gujarat import process_gujarat
+from subsidy_punjab import process_punjab
+import os
+
+app = Flask(__name__)
+
+@app.route("/subsidy", methods=["POST"])
+def calculate_subsidy():
+    data = request.get_json(force=True)
+    State = data.get("State")
+    enterprise_size = data.get("Enterprise size")
+
+    if not State or not enterprise_size:
+        return jsonify({"error": "Missing required fields in input"}), 400
+
+    if State == "Rajasthan":
+        result = process_rajasthan(data)
+    elif State == "Haryana":
+        result = process_haryana(data)
+    elif State == "Uttarpradesh": 
+        if enterprise_size in ["Large", "Mega", "Ultra Mega", "Super Mega", "Ultra-Mega"]:
+            result = process_up(data)
+        elif enterprise_size in ["Micro", "Small", "Medium"]:
+            result = process_up_msme(data)
+    elif State == "Madhyapradesh":
+        result = process_madhyapradesh(data)
+    elif State == "Karnataka":
+        result = process_karnataka(data)
+    elif State == "Tamilnadu":
+        result = process_tamilnadu(data)
+    elif State == "Maharashtra":
+        result = process_maharashtra(data)
+    elif State == "Gujarat":
+        result = process_gujarat(data)
+    elif State == "Punjab":
+        result = process_punjab(data)
+    else:
+        return jsonify({"error": "Unsupported state selected"}), 400
+
+    return jsonify(result)
+
+@app.route("/download_pdf/<filename>", methods=["GET"])
+def download_pdf(filename):
+    path = os.path.join(os.getcwd(), filename)
+    return send_file(
+        path,
+        as_attachment=True,
+        download_name=filename,
+        mimetype="application/pdf"
+    )
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ subsidy_madhyapradesh.py
@@ -0,0 +1,71 @@
+from report_madhyapradesh import generate_report_mp
+
+# No zonification in Madhyapradesh
+# interest rate is new input 
+
+# Calculation logic
+def calculate_subsidy(plant_machinery, building_civil_work,
+                      term_loan_amount, interest_rate):
+
+    capital_investment = plant_machinery + building_civil_work
+
+    # Capital Subsidy depends on Eligible Fixed Capital Investment(EFCI)
+    if capital_investment<=300000000:
+        capital_subsidy= min(0.40 * capital_investment, 120000000)
+    elif 300000000 <= capital_investment<= 500000000:
+        capital_subsidy= min(0.40 * capital_investment, 200000000)
+    elif 500000000 <= capital_investment<= 1000000000:
+        capital_subsidy= min(0.27 * capital_investment, 270000000)
+    elif 1000000000<= capital_investment<=5000000000:
+        capital_subsidy= min(0.15 * capital_investment, 750000000)
+    elif 5000000000<= capital_investment<=10000000000:
+        capital_subsidy= min(0.13 * capital_investment, 1300000000)
+    elif 10000000000<= capital_investment<=20000000000:
+        capital_subsidy= min(0.10 * capital_investment, 2000000000)
+    else:
+        capital_subsidy = 2000000000
+
+    # No Stamp Duty Subsidy in Madhyapradesh
+
+    # Interest Subsidy
+    annual_interest = term_loan_amount * 0.06 * 7
+    interest_paid =  interest_rate * term_loan_amount * 7
+    interest_subsidy = min(annual_interest, interest_paid, 100000000) #max 10 crore
+
+    # Total subsidy 
+    total_subsidy = capital_subsidy + interest_subsidy
+     
+    return {
+        "capital_investment_subsidy": round(capital_subsidy, 2),
+        "interest_subsidy": round(interest_subsidy, 2),
+        "total_subsidy": round(total_subsidy, 2)
+    }    
+
+def process_madhyapradesh(data):
+    try:
+        # Extract values
+        plant_machinery = float(data["Plant and Machinery Investment"])
+        building_civil_work = float(data["Building and Civil Work Investment"])
+        term_loan_amount = float(data["Term Loan Amount"])
+        interest_rate = float(data["Interest Rate"])
+
+        # Perform calculations
+        result = calculate_subsidy(
+            plant_machinery,
+            building_civil_work,
+            term_loan_amount,
+            interest_rate
+        )
+
+        # Generate report
+        pdf_path = generate_report_mp(data, result)
+
+        return {
+            "subsidy_result": result,
+            "report_path": pdf_path
+        }
+
+    except Exception as e:
+        return {
+            "error": f"Error processing Madhyapradesh subsidy: {str(e)}"
+        }
