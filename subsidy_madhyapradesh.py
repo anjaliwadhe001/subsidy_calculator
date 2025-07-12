74270efe4fcd0a1c333f44e10a7643b100bbe92c
anjaliwadhe001 <84492122+anjaliwadhe001@users.noreply.github.com>
2025-07-12 11:44:54 +0530

    Add files via upload

--- subsidy_madhyapradesh.py
+++ subsidy_madhyapradesh.py
@@ -1,13 +1,15 @@
 from report_madhyapradesh import generate_report_mp
+import os
 
 # No zonification in Madhyapradesh
 # interest rate is new input 
 
 # Calculation logic
 def calculate_subsidy(plant_machinery, building_civil_work,
-                      term_loan_amount, interest_rate):
+                      term_loan_amount = 0, interest_rate = 0):
 
     capital_investment = plant_machinery + building_civil_work
+    print("Capital investment", capital_investment)
 
     # Capital Subsidy depends on Eligible Fixed Capital Investment(EFCI)
     if capital_investment<=300000000:
@@ -25,29 +27,46 @@ def calculate_subsidy(plant_machinery, building_civil_work,
     else:
         capital_subsidy = 2000000000
 
-    # No Stamp Duty Subsidy and SGST reimbursement  in Madhyapradesh
+    print("Capital subsidy", capital_subsidy)
+
+    # No Stamp Duty Subsidy in Madhyapradesh
 
     # Interest Subsidy
-    annual_interest = term_loan_amount * 0.06 * 7 #6% interest rate for 7 years 
-    interest_paid =  interest_rate * term_loan_amount * 7 #interest paid by user 
+    annual_interest = term_loan_amount * 0.06 * 7
+    interest_paid =  interest_rate * term_loan_amount * 7
     interest_subsidy = min(annual_interest, interest_paid, 100000000) #max 10 crore
 
+    print("Interest Subsidy", interest_subsidy)
     # Total subsidy 
     total_subsidy = capital_subsidy + interest_subsidy
-     
+    print("Total subsidy: ", total_subsidy)
+
     return {
         "capital_investment_subsidy": round(capital_subsidy, 2),
         "interest_subsidy": round(interest_subsidy, 2),
         "total_subsidy": round(total_subsidy, 2)
     }    
 
+def safe_float(value):
+    try:
+        return float(str(value).replace(",", "").replace("Rs.", "").strip())
+    except:
+        return 0.0
+
+
 def process_madhyapradesh(data):
     try:
+        
+        print("A")
         # Extract values
-        plant_machinery = float(data["Plant and Machinery Investment"])
-        building_civil_work = float(data["Building and Civil Work Investment"])
-        term_loan_amount = float(data["Term Loan Amount"])
-        interest_rate = float(data["Interest Rate"])
+        plant_machinery = safe_float(data["Plant and Machinery Investment"])
+        print("B: ", data.get("Plant and Machinery Investment"))
+        building_civil_work = safe_float(data["Building and Civil Work Investment"])
+        print("C: ", building_civil_work)
+        term_loan_amount = safe_float(data["Term Loan Amount"])
+        print("D: ", term_loan_amount)
+        interest_rate = safe_float(data["Interest Rate"])
+        print("E: ", interest_rate)
 
         # Perform calculations
         result = calculate_subsidy(
@@ -56,16 +75,21 @@ def process_madhyapradesh(data):
             term_loan_amount,
             interest_rate
         )
+        print("Calculations: ", result)
 
         # Generate report
         pdf_path = generate_report_mp(data, result)
+        filename = os.path.basename(pdf_path)
 
+        print('PDF link', pdf_path)
+        
         return {
             "subsidy_result": result,
-            "report_path": pdf_path
+            "report_path": f"/download_pdf/{filename}"
         }
 
-    except Exception as e:
+    except Exception as e: 
+        print("Error:", str(e))
         return {
             "error": f"Error processing Madhyapradesh subsidy: {str(e)}"
         }
